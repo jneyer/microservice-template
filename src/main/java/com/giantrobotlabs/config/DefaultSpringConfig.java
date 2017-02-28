c8d9d5bcb095cf3b6afbb7772a08bdc24aec1c13
John Neyer <jneyer@captechconsulting.com>
2017-02-28 15:40:51 -0500

    Setting up default properties

--- src/main/java/com/giantrobotlabs/config/DefaultSpringConfig.java
+++ src/main/java/com/giantrobotlabs/config/DefaultSpringConfig.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
@@ -23,6 +24,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@PropertySource("classpath:default.properties")
 public class DefaultSpringConfig {
 
 	@Autowired
